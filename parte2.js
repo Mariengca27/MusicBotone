ed05397624de097713eb5753b9b9eb9843e3f774
Mari <53942555+Mariengca27@users.noreply.github.com>
2020-12-14 21:30:01 -0300

    Arquivos SEPARADOS
    Os códigos separados de cada parte: CSS e JavaScript.

---
+++ parte2.js
@@ -0,0 +1,44 @@
+var comecar = document.getElementById('comecar');
+var interval;
+var end1 = document.getElementById('end');
+var interval2;
+
+//Lógica do botão para iniciar a música 
+comecar.addEventListener('click', function(){
+
+
+
+
+
+//Lógica do botão para iniciar a música 
+interval = setInterval(function(){
+   
+           document.getElementById("sound").play();
+           clearInterval(interval);
+
+    	
+
+})
+
+})
+
+
+
+//Comando de pausa da música DANDO ERRO, arrumar depois ¬¬
+end1.addEventListener('click', function(){
+
+
+
+
+
+//Lógica do botão para iniciar a música 
+interval2 = setInterval2(function(){
+   
+           document.getElementById("sound").pause();
+           clearInterval(interval2);
+
+    	
+
+})
+
+})
